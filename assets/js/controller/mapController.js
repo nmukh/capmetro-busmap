a1371d06b8d6f9f57ea74bf54a6a4c74e4d8120a
Nishat Mukherji <nish@nishsin.com>
2015-04-30 15:35:04 -0500

    Refresh location every 30 seconds. Fix #1

--- assets/js/controller/mapController.js
+++ assets/js/controller/mapController.js
@@ -1,22 +1,32 @@
-app.controller('mapController',['$scope','$http',function($scope,$http){
+app.controller('mapController',['$scope','$http', '$timeout', function($scope,$http,$timeout){
 	$scope.center = {
         lat: 30.2909,
         lng: -97.7330,
         zoom: 13
     };
 	$scope.markers = [];
-    $http.get('https://data.texas.gov/resource/9e7h-gz56.json').success(function(data) {
-    	//console.log(data);
-    	$scope.markers = [];
-    	data.forEach(function(bus){
-    		locationTemp = bus.location.split(',');
-            vehicleId = bus.vehicleid
-    		$scope.markers.push({
-    			lat : parseFloat(locationTemp[0]),
-    			lng : parseFloat(locationTemp[1]),
-    			message : vehicleId
-    		});
-    	});
-    });
+    $scope.getData = function(){
+        $http.get('https://data.texas.gov/resource/9e7h-gz56.json').success(function(data) {
+        	//console.log(data);
+        	$scope.markers = [];
+        	data.forEach(function(bus){
+        		locationTemp = bus.location.split(',');
+                vehicleId = bus.vehicleid
+        		$scope.markers.push({
+        			lat : parseFloat(locationTemp[0]),
+        			lng : parseFloat(locationTemp[1]),
+        			message : vehicleId
+        		});
+        	});
+            console.log('Fetched data');
+        });
+    }
+    $scope.intervalFunction = function(){
+        $timeout(function() {
+          $scope.getData();
+          $scope.intervalFunction();
+        }, 3000)
+      };
 
+    $scope.intervalFunction();
 }]);
